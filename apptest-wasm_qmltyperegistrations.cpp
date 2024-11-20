ee60740ee15fd78a13e84b7c19d9011af0cf2a44
cld-marginean <cld.marginean@yahoo.com>
2024-11-20 11:54:04 +0100

    test wasm files

---
+++ apptest-wasm_qmltyperegistrations.cpp
@@ -0,0 +1,24 @@
+/****************************************************************************
+** Generated QML type registration code
+**
+** WARNING! All changes made in this file will be lost!
+*****************************************************************************/
+
+#include <QtQml/qqml.h>
+#include <QtQml/qqmlmoduleregistration.h>
+
+
+
+#if !defined(QT_STATIC)
+#define Q_QMLTYPE_EXPORT Q_DECL_EXPORT
+#else
+#define Q_QMLTYPE_EXPORT
+#endif
+Q_QMLTYPE_EXPORT void qml_register_types_Test_WASM()
+{
+    QT_WARNING_PUSH QT_WARNING_DISABLE_DEPRECATED
+    QT_WARNING_POP
+    qmlRegisterModule("Test-WASM", 1, 0);
+}
+
+static const QQmlModuleRegistration testWASMRegistration("Test-WASM", qml_register_types_Test_WASM);
